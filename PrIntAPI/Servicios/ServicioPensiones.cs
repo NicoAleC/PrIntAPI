16f8514bc0fdfb1c184ed64c9a5f8f400efe5c26
Nicolas Carrasco <chincholax@gmail.com>
2018-11-04 23:02:34 -0400

    develop: agregados los servicios para Pensiones y Prestamos

---
+++ PrIntAPI/Servicios/ServicioPensiones.cs
@@ -0,0 +1,91 @@
+﻿using PrIntAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrIntAPI.Servicios
+{
+    class ServicioPensiones
+    {
+        public DB lista;
+
+        public ServicioPensiones()
+        {
+            lista = DB.instance;
+        }
+
+        public int GetIndex(string codigo_est)
+        {
+            return lista.PensionesLista.FindIndex((x => x.codigo_est == int.Parse(codigo_est)));
+        }
+
+        public bool Verificar(Pensiones c)
+        {
+            return lista.PensionesLista.Exists((x => x.codigo_est == c.codigo_est));
+        }
+
+        public bool Crear(Pensiones c)
+        {
+            bool existe = !Verificar(c);
+
+            if (existe)
+            {
+                lista.PensionesLista.Add(c);
+            }
+
+            return existe;
+        }
+
+        public List<Pensiones> Leer()
+        {
+            return lista.PensionesLista;
+        }
+
+        public bool Actualizar(string key, Pensiones c)
+        {
+            int index = GetIndex(key);
+            bool existe = true;
+            if (!key.Equals(c.codigo_est))
+            {
+                if (GetIndex(c.codigo_est + "") != -1)
+                {
+                    existe = true;
+                }
+                else
+                {
+                    existe = true;
+                }
+            }
+            if (existe)
+            {
+                if (index != -1)
+                {
+                    lista.PensionesLista[index] = c;
+                }
+                else
+                {
+                    existe = false;
+                }
+            }
+            return existe;
+        }
+
+        public bool Eliminar(string key)
+        {
+            bool eliminado = true;
+            int index = GetIndex(key);
+            if (index != -1)
+            {
+                lista.PensionesLista.RemoveAt(index);
+            }
+            else
+            {
+                eliminado = false;
+            }
+
+            return eliminado;
+        }
+    }
+}
---
+++ PrIntAPI/Servicios/ServicioPrestamos.cs
@@ -0,0 +1,91 @@
+﻿using PrIntAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrIntAPI.Servicios
+{
+    class ServicioPrestamos
+    {
+        public DB lista;
+
+        public ServicioPrestamos()
+        {
+            lista = DB.instance;
+        }
+
+        public int GetIndex(string codigo_est)
+        {
+            return lista.PrestamosLista.FindIndex((x => x.codigo_est == int.Parse(codigo_est)));
+        }
+
+        public bool Verificar(Prestamos c)
+        {
+            return lista.PrestamosLista.Exists((x => x.codigo_est == c.codigo_est));
+        }
+
+        public bool Crear(Prestamos c)
+        {
+            bool existe = !Verificar(c);
+
+            if (existe)
+            {
+                lista.PrestamosLista.Add(c);
+            }
+
+            return existe;
+        }
+
+        public List<Prestamos> Leer()
+        {
+            return lista.PrestamosLista;
+        }
+
+        public bool Actualizar(string key, Prestamos c)
+        {
+            int index = GetIndex(key);
+            bool existe = true;
+            if (!key.Equals(c.codigo_est))
+            {
+                if (GetIndex(c.codigo_est + "") != -1)
+                {
+                    existe = true;
+                }
+                else
+                {
+                    existe = true;
+                }
+            }
+            if (existe)
+            {
+                if (index != -1)
+                {
+                    lista.PrestamosLista[index] = c;
+                }
+                else
+                {
+                    existe = false;
+                }
+            }
+            return existe;
+        }
+
+        public bool Eliminar(string key)
+        {
+            bool eliminado = true;
+            int index = GetIndex(key);
+            if (index != -1)
+            {
+                lista.PrestamosLista.RemoveAt(index);
+            }
+            else
+            {
+                eliminado = false;
+            }
+
+            return eliminado;
+        }
+    }
+}
